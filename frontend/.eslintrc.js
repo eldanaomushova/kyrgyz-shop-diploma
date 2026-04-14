09e01a5a3caa39752a3600159a36400ce83f3498
eldanaomushova <eldana956@gmail.com>
2026-04-14 12:21:35 +0600

    Add some chnages before deployment4

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,11 +2,10 @@ module.exports = {
     extends: ["react-app", "react-app/jest"],
     rules: {
         "no-unused-vars": "error",
-        "react-hooks/exhaustive-deps": "error",
+        "react-hooks/exhaustive-deps": "warn",
         "jsx-a11y/anchor-is-valid": "error",
         "no-console": "warn",
         "no-debugger": "error",
-        "react/prop-types": "warn",
         "no-duplicate-imports": "error",
         "no-unreachable": "error",
         "react/prop-types": "off",

6f5224963954451cf0a047cb7969764946bb9482
eldanaomushova <eldana956@gmail.com>
2026-05-18 19:34:40 +0600

    Fix ALLOWED_HOSTS for Railway

--- core/settings.py
+++ core/settings.py
@@ -40,11 +40,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000', 
     'https://run.app'
 ]
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '.run.app',  
-]
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
